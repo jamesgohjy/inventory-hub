d6ab20ed9efbed3f392cfbcbf4ec2db114418984
jamesgohjy <jamesgohjy@gmail.com>
2026-09-12 00:57:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// AV Inventory Hub V6.22
-const APP_VERSION='6.22';
+// AV Inventory Hub V6.23
+const APP_VERSION='6.23';
 const CFG = window.INVENTORY_CONFIG || {mode:'local'};
 const $ = (id)=>document.getElementById(id);
 const esc = (s='') => String(s).replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
